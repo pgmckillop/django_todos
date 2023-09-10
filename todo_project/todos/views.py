fc8a2f8fb6140a5ffad6cf99f1d25266a8682c51
Paul McKillop <pgmckillop@gmail.com>
2023-09-10 10:49:06 +0100

    Display pages index and about with path, view and template

--- todo_project/todos/views.py
+++ todo_project/todos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Home page index.html
+def index(request):
+    return render(request, 'todos/index.html', {})
+
+def about(request):
+    return render(request, 'todos/about.html', {})
