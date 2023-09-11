89e1a8ea2ad09b8b31804d09c4916d8ecbc05f48
Paul McKillop <pgmckillop@gmail.com>
2023-09-11 18:13:23 +0100

    Website structure complete.

--- todo_project/todos/views.py
+++ todo_project/todos/views.py
@@ -5,4 +5,14 @@ def index(request):
     return render(request, 'todos/index.html', {})
 
 def about(request):
-    return render(request, 'todos/about.html', {})
+    wholename = "Paul McKillop"
+    forename = "Paul"
+    surname = "McKillop"
+    organisation = "The Isle of Wight College"
+    context = {
+        'name': wholename,
+        'organisation': organisation,
+        'forename': forename,
+        'surname': surname
+    }
+    return render(request, 'todos/about.html', context)
