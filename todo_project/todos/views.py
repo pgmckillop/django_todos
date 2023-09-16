575d275adcce83d495a37526d33653bb7f1256cb
Paul McKillop <pgmckillop@gmail.com>
2023-09-16 13:34:55 +0100

    Edit and cross off or uncross complete

--- todo_project/todos/views.py
+++ todo_project/todos/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Home page index.html
 def index(request):
@@ -10,6 +12,7 @@ def index(request):
         if form.is_valid():
             form.save()
             all_items = List.objects.all
+            messages.success(request, 'Item has been added')
             return render(request, 'todos/index.html', {'all_items': all_items})
     else:
         all_items = List.objects.all
@@ -27,3 +30,23 @@ def about(request):
         'surname': surname
     }
     return render(request, 'todos/about.html', context)
+
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, ('Item has been deleted!'))
+    return redirect(index)
+
+def cross_off(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+    return redirect('index')
+
+
+def uncross(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+    return redirect('index')
